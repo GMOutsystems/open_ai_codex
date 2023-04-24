67a798a4f6e3ee9b9033be75944d0db38c88f94e
gbnm <goncalo.martins@outsystems.com>
2023-04-25 00:01:35 +0100

    update server URL

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot response 
   // Initially http://localhost:5000 after we created the server
-  const response = await fetch('https://gm-open-ai-codex1.onrender.com',{
+  const response = await fetch('https://gm-open-ai-codex1.onrender.com/',{
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
